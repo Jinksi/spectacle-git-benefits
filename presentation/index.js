ddbc7a59801d2866c47753dd9f7b64d9d40a7252
Jinksi <ericjinks@gmail.com>
2016-04-12 21:03:18 +1000

    Slides are go!

--- presentation/index.js
+++ presentation/index.js
@@ -37,65 +37,134 @@ require("spectacle/lib/themes/default/index.css");
 
 const images = {
   kat: require("../assets/kat.png"),
-  git: require("../assets/git.png")
+  git: require("../assets/git.png"),
+  coolStory: require("../assets/cool-story.gif"),
+  bb: require("../assets/bb.png"),
+  gh: require("../assets/gh.png"),
+  sourcetreeApp: require("../assets/sourcetree-app.png"),
+  githubApp: require("../assets/github-app.png")
 };
 
 preloader(images);
 
 const theme = createTheme({
   primary: "rgba(255,81,81, 1)",
-  black: "#212121"
+  black: "#151515"
+}, {
+  primary: "'Josefin Sans', sans-serif "
 });
 
 export default class Presentation extends React.Component {
   render() {
     return (
       <Spectacle theme={theme}>
-        <Deck transition={["zoom", "slide"]} transitionDuration={500}>
+        <Deck progress="bar" transition={["slide"]} transitionDuration={500}>
 
-          <Slide transition={["zoom"]} bgColor="primary">
-            <Image src={images.git.replace("/", "")} margin="0px auto 40px" height="293px"/>
+          <Slide transition={["slide"]} bgColor="primary">
+            <Image src={images.git.replace("/", "")} margin="0px auto 40px" width="300px"/>
             <Heading size={1} caps lineHeight={1} textColor="black">
               Git Benefits
             </Heading>
-            <Heading size={1} caps>
+            <Text caps>
               Why should I use git?
-            </Heading>
+            </Text>
+            <br/>
             <Link href="https://github.com/Jinksi/spectacle-git-benefits">
               <Text bold caps textColor="tertiary">View on Github</Text>
             </Link>
           </Slide>
 
-          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Heading size={2} caps fit textColor="primary" textFont="primary">
+          <Slide bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
+            <Heading size={1} caps fit textColor="primary" textFont="primary" margin="0 0 25px">
               What is git?
             </Heading>
-            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
+            <BlockQuote textColor="white" size={1}>
+              <Quote textSize="3rem" textColor="white">
+                Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.
+              </Quote>
+              <Cite><Link textColor="primary" href="https://git-scm.com">git-scm.com</Link></Cite>
+            </BlockQuote>
+          </Slide>
+
+          <Slide bgImage={images.coolStory}>
+            <Heading>COOL</Heading>
+          </Slide>
+
+          <Slide bgColor="black">
+            <Heading size={1} caps fit textColor="primary" textFont="primary" margin="0 0 25px">
+              How does Git benefit me?
+            </Heading>
+          </Slide>
+
+          <Slide id="benefit-1">
+            <Heading size={1} caps>Benefit #1</Heading>
+            <Text textColor="white" margin="5vh auto" bgColor="black" padding="3rem">
+              Git keeps “snapshots” of your project
+            </Text>
+            <Text>
+              Every time you “commit” a change, you describe it.
+            </Text>
+            <Text>
+              You can go back through each commit in your project's history to view or undo you changes.
+            </Text>
+          </Slide>
+
+          <Slide id="benefit-2">
+            <Heading size={1} caps>Benefit #2</Heading>
+            <Text textColor="white" margin="5vh auto" bgColor="black" padding="3rem">
+              You can store your git repo securely “in the cloud”
+            </Text>
+            <Text>
+              <Image display="inline-block" height="40px" src={images.gh} />
+              <Image display="inline-block" height="40px" src={images.bb} />
+            </Text>
+            <Text>
+              Your work is safe if your computer explodes or office burns down.
+            </Text>
+            <Text>
+              Easy to share & “clone” project. You can push/pull changes to multiple computers.
+            </Text>
+          </Slide>
+
+          <Slide id="benefit-3">
+            <Heading size={1} caps>Benefit #3</Heading>
+            <Text textColor="white" margin="5vh auto" bgColor="black" padding="3rem">
+              Multiple people can work on the same project simultaneously
+            </Text>
+            <Text>
+              and the changes can “merged” with ease.
+            </Text>
+            <Text>
+              No risk of overwriting each other's code.
+            </Text>
+          </Slide>
+
+          <Slide bgColor="black">
+            <Heading textColor="primary" fit caps>How do I use Git?</Heading>
             <Appear>
-              <Text>
-                Git keeps “snapshots” of your project, one line at a time.
-              </Text>
-            </Appear>
-            <Appear>
-              <Text>
-                These changed are kept in a “repository”
-              </Text>
-            </Appear>
-            <Appear>
-              <Text>
-                Can view your previous changes / revert.
-              </Text>
-            </Appear>
-            <Appear>
-              <Text>
-                Multiple users can work on the project simultaneously and the changes can “merged” cleanly.
-              </Text>
+              <Heading caps textColor="white">1. Use a GUI</Heading>
             </Appear>
           </Slide>
 
+          <Slide bgColor="black">
+            <Link bold caps href="https://desktop.github.com/" textColor="white">Github App
+              <Image margin="25px 0" src={images.githubApp} width="100%"/>
+            </Link>
+          </Slide>
+
+          <Slide bgColor="black">
+            <Link bold caps href="https://www.sourcetreeapp.com/" textColor="white">Sourcetree
+              <Image margin="25px 0" src={images.sourcetreeApp} width="100%"/>
+            </Link>
+          </Slide>
+
+          <Slide bgColor="black">
+            <Heading textColor="primary" fit caps>How do I use Git?</Heading>
+            <Heading fit caps textColor="white">2. Use the command line</Heading>
+          </Slide>
+
           <CodeSlide
-            lang="bash"
+            lang="js"
             transition={["slide"]}
             code={require("raw!../assets/code.example")}
             ranges={[
@@ -103,12 +172,12 @@ export default class Presentation extends React.Component {
             ]}
             />
 
-          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Heading size={2} caps fit textColor="primary" textFont="primary">
-              Wait what?
+          <Slide bgColor="black">
+            <Heading size={1} caps fit textColor="primary" textFont="primary" margin="0 0 25px">
+              Git & Wordpress
             </Heading>
           </Slide>
+
 
         </Deck>
       </Spectacle>
