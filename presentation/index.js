2ec114b8fcf9fc7f2a6a39fed4359a17b4a2358c
Eric Jinks <ericjinks@gmail.com>
2016-04-12 17:20:45 +1000

    Adding info

--- presentation/index.js
+++ presentation/index.js
@@ -36,16 +36,15 @@ require("spectacle/lib/themes/default/index.css");
 
 
 const images = {
-  city: require("../assets/city.jpg"),
   kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
+  git: require("../assets/git.png")
 };
 
 preloader(images);
 
 const theme = createTheme({
-  primary: "#ff4081"
+  primary: "rgba(255,81,81, 1)",
+  black: "#212121"
 });
 
 export default class Presentation extends React.Component {
@@ -55,28 +54,52 @@ export default class Presentation extends React.Component {
         <Deck transition={["zoom", "slide"]} transitionDuration={500}>
 
           <Slide transition={["zoom"]} bgColor="primary">
-            <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Spectacle
+            <Image src={images.git.replace("/", "")} margin="0px auto 40px" height="293px"/>
+            <Heading size={1} caps lineHeight={1} textColor="black">
+              Git Benefits
             </Heading>
-            <Heading size={1} fit caps>
-              A ReactJS Presentation Library
+            <Heading size={1} caps>
+              Why should I use git?
             </Heading>
-            <Heading size={1} fit caps textColor="black">
-              Where You Can Write Your Decks In JSX
-            </Heading>
-            <Link href="https://github.com/FormidableLabs/spectacle">
+            <Link href="https://github.com/Jinksi/spectacle-git-benefits">
               <Text bold caps textColor="tertiary">View on Github</Text>
             </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
+          </Slide>
+
+          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
+            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
+            <Heading size={2} caps fit textColor="primary" textFont="primary">
+              What is git?
+            </Heading>
+            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
+            <Appear>
+              <Text>
+                Git keeps “snapshots” of your project, one line at a time.
+              </Text>
+            </Appear>
+            <Appear>
+              <Text>
+                These changed are kept in a “repository”
+              </Text>
+            </Appear>
+            <Appear>
+              <Text>
+                Can view your previous changes / revert.
+              </Text>
+            </Appear>
+            <Appear>
+              <Text>
+                Multiple users can work on the project simultaneously and the changes can “merged” cleanly.
+              </Text>
+            </Appear>
           </Slide>
 
           <CodeSlide
-            lang="jsx"
-            transition={["zoom"]}
+            lang="bash"
+            transition={["slide"]}
             code={require("raw!../assets/code.example")}
             ranges={[
-              { loc: [0, 1], title: "Import dependencies" },
-              { loc: [2, 9], title: "Stateless Component" }
+              { loc: [0, 1], title: "Import dependencies" }
             ]}
             />
 
@@ -85,13 +108,6 @@ export default class Presentation extends React.Component {
             <Heading size={2} caps fit textColor="primary" textFont="primary">
               Wait what?
             </Heading>
-          </Slide>
-
-          <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              Your presentations are interactive
-            </Heading>
-            <Interactive/>
           </Slide>
 
         </Deck>
